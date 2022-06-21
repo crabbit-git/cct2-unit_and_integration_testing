0c658b5def4f0ec5a2347797b6e1e9f9cccea3f4
crabbit-git <98990717+crabbit-git@users.noreply.github.com>
2022-06-21 10:22:09 +0100

    First 2 tests pass

--- src/tests/Calculator.test.js
+++ src/tests/Calculator.test.js
@@ -6,16 +6,26 @@ import Adapter from 'enzyme-adapter-react-16';
 configure({adapter: new Adapter()});
 
 describe('Calculator', () => {
-  let container;
+  let container, button1, button4, buttonAdd, buttonEquals, runningTotal;
   beforeEach(() => {
     container = mount(<Calculator/>)
+    button1 = container.find('#number1');
+    button4 = container.find('#number4');
+    buttonAdd = container.find('#operator-add');
+    buttonEquals = container.find('#operator-equals');
+    runningTotal = container.find('#running-total');
   })
 
   it('should change running total on number enter', () => {
-    const button4 = container.find('#number4');
-    const runningTotal = container.find('#running-total');
     button4.simulate('click');
     expect(runningTotal.text()).toEqual('4');
   })
-})
 
+  it('should be able to accurately add two integers', () => {
+    button1.simulate('click');
+    buttonAdd.simulate('click');
+    button4.simulate('click');
+    buttonEquals.simulate('click');
+    expect(runningTotal.text()).toEqual('5');
+  })
+})
