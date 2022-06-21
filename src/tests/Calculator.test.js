dbc310b28613d91f8691319af47a1bf5e9173c1d
crabbit-git <98990717+crabbit-git@users.noreply.github.com>
2022-06-21 10:27:46 +0100

    Subtraction test passes

--- src/tests/Calculator.test.js
+++ src/tests/Calculator.test.js
@@ -6,20 +6,26 @@ import Adapter from 'enzyme-adapter-react-16';
 configure({adapter: new Adapter()});
 
 describe('Calculator', () => {
-  let container, button1, button4, buttonAdd, buttonEquals, runningTotal;
+  let container,
+      button1, button4, button7,
+      buttonAdd, buttonSubtract, buttonEquals,
+      runningTotal;
+
   beforeEach(() => {
     container = mount(<Calculator/>)
     button1 = container.find('#number1');
     button4 = container.find('#number4');
+    button7 = container.find('#number7');
     buttonAdd = container.find('#operator-add');
+    buttonSubtract = container.find('#operator-subtract');
     buttonEquals = container.find('#operator-equals');
     runningTotal = container.find('#running-total');
-  })
+  });
 
   it('should change running total on number enter', () => {
     button4.simulate('click');
     expect(runningTotal.text()).toEqual('4');
-  })
+  });
 
   it('should be able to accurately add two integers', () => {
     button1.simulate('click');
@@ -27,5 +33,13 @@ describe('Calculator', () => {
     button4.simulate('click');
     buttonEquals.simulate('click');
     expect(runningTotal.text()).toEqual('5');
-  })
-})
+  });
+
+  it('should be able to accurately subtract one integer from another', () => {
+    button7.simulate('click');
+    buttonSubtract.simulate('click');
+    button4.simulate('click');
+    buttonEquals.simulate('click');
+    expect(runningTotal.text()).toEqual('3');
+  });
+});
