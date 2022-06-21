bace887573434e62d426d4a5d8b8b3a1b5478cc2
crabbit-git <98990717+crabbit-git@users.noreply.github.com>
2022-06-21 10:31:05 +0100

    Multiplication test passes

--- src/tests/Calculator.test.js
+++ src/tests/Calculator.test.js
@@ -7,17 +7,20 @@ configure({adapter: new Adapter()});
 
 describe('Calculator', () => {
   let container,
-      button1, button4, button7,
-      buttonAdd, buttonSubtract, buttonEquals,
+      button1, button3, button4, button5, button7,
+      buttonAdd, buttonSubtract, buttonMultiply, buttonEquals,
       runningTotal;
 
   beforeEach(() => {
     container = mount(<Calculator/>)
     button1 = container.find('#number1');
+    button3 = container.find('#number3');
     button4 = container.find('#number4');
+    button5 = container.find('#number5');
     button7 = container.find('#number7');
     buttonAdd = container.find('#operator-add');
     buttonSubtract = container.find('#operator-subtract');
+    buttonMultiply = container.find('#operator-multiply');
     buttonEquals = container.find('#operator-equals');
     runningTotal = container.find('#running-total');
   });
@@ -42,4 +45,12 @@ describe('Calculator', () => {
     buttonEquals.simulate('click');
     expect(runningTotal.text()).toEqual('3');
   });
+
+  it('should be able to accurately multiply two integers', () => {
+    button3.simulate('click');
+    buttonMultiply.simulate('click');
+    button5.simulate('click');
+    buttonEquals.simulate('click');
+    expect(runningTotal.text()).toEqual('15');
+  })
 });
