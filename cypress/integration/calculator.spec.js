6bbdc4abb82334eacc0793a648d726e554d2d46a
crabbit-git <98990717+crabbit-git@users.noreply.github.com>
2022-06-23 22:32:38 +0100

    Large number test done

--- cypress/integration/calculator.spec.js
+++ cypress/integration/calculator.spec.js
@@ -38,4 +38,41 @@ describe("Calculator", () => {
     cy.get('.display').should('contain', '5.8');
   });
 
+  it('should return valid results for exceptionally large numbers', () => {
+    cy.get('#number9').click();
+    cy.get('#number9').click();
+    cy.get('#number9').click();
+    cy.get('#number9').click();
+    cy.get('#operator-multiply').click();
+    cy.get('#number9').click();
+    cy.get('#number9').click();
+    cy.get('#number9').click();
+    cy.get('#number9').click();
+    cy.get('#operator-multiply').click();
+    cy.get('#number9').click();
+    cy.get('#number9').click();
+    cy.get('#number9').click();
+    cy.get('#number9').click();
+    cy.get('#operator-multiply').click();
+    cy.get('#number9').click();
+    cy.get('#number9').click();
+    cy.get('#number9').click();
+    cy.get('#number9').click();
+    cy.get('#operator-multiply').click();
+    cy.get('#number9').click();
+    cy.get('#number9').click();
+    cy.get('#number9').click();
+    cy.get('#number9').click();
+    cy.get('#operator-equals').click();
+    cy.get('.display').should('contain', '99950009999000040000');
+  });
+
+  // it('should return "Undefined" if user attempts to divide by 0', () => {
+  //   cy.get('#number1').click();
+  //   cy.get('#operator-divide').click();
+  //   cy.get('#number0').click();
+  //   cy.get('#operator-equals').click();
+  //   cy.get('.display').should('contain', 'Undefined');
+  // });
+
 });
